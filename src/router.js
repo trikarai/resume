e401e33206c79eda5fffe8f09b14311ec13c591b
Tri S <mas.trisutrisno@gmail.com>
2019-06-19 11:33:08 +0700

    Update router.js
    update router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import Resume from './views/resume/Resume.vue'
+import Maintenance from './views/Maintenance.vue'
 
 Vue.use(Router)
 
@@ -10,16 +12,22 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Mainteance',
+      component: Maintenance
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/resume',
+      name: 'resume',
+      props: true,
+      component: Resume
     }
+    // {
+    // path: '/about',
+    // name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })

d0daa114a926f9a9d07b09b2543d81e95857188e
Tri S <mas.trisutrisno@gmail.com>
2019-06-19 11:33:28 +0700

    Update main.js
    add font awesome dependency

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faFacebook, faGithub, faInstagram, faStrava, faWhatsapp, faHtml5, faVuejs, faAndroid, faBootstrap, faJs, faNpm, faPhp, faCss3 } from '@fortawesome/free-brands-svg-icons'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+library.add(faTwitter, faFacebook, faGithub, faInstagram, faStrava, faWhatsapp, faHtml5, faVuejs, faAndroid, faBootstrap, faJs, faNpm, faPhp, faCss3)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
